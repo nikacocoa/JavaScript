89a465fb770fe29312d78bcb634e55081a3ba12a
EC2 Default User <ec2-user@ip-172-31-1-125.ap-northeast-1.compute.internal>
2024-07-14 17:24:27 +0000

    課題７

---
+++ .c9/JS/kadai7.js
@@ -0,0 +1,12 @@
+let fruits = {
+    apple: "りんご",
+    strawberry: "いちご",
+    grape: "ぶどう",
+    lemon: "れもん"
+};
+
+console.log(fruits.grape);
+
+console.log(Object.keys(fruits));
+
+console.log(Object.values(fruits));
